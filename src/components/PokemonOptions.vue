e8ea16813277ae199088d2a542247ee9becdf8a7
semarkata <semarkata@gmail.com>
2022-11-12 21:01:11 +0100

    Removing HelloWorld component, removing HelloWorld component test, adding PokemonOptions component and PokemonPicture component

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3> Pokemon Options </h3>
+</template>
+
+<script>
+export default {
+  name: "PokemonPicture"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1> Who's that Pokemon? </h1>
+  <PokemonPicture/>
+  <PokemonOptions/>
+  <!--  TODO: Options -->
+</template>
+<script>
+
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: {PokemonPicture, PokemonOptions},
+}
+
+</script>
